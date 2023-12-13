21dee61e3076ad75349f4859b45871ab527bdc51
AnnaCristyna <annacrismb@gmail.com>
2023-12-13 09:00:19 -0300

    fix: add modular and components

--- src/app/components/profile/profile.component.ts
+++ src/app/components/profile/profile.component.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-profile',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './profile.component.html',
   styleUrl: './profile.component.css'
 })
 export class ProfileComponent {
-
+  stars: number;
+  
+  constructor() {
+    this.stars = 5;
+  }
+  
 }
